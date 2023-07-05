e72d0c1bb5f7279760029eb4ac40c71a0738f4c9
kadr <kadr1987@gmail.com>
2023-07-05 17:12:18 +0400

    DTO для методов сервиса продукта

--- internal/service/product.go
+++ internal/service/product.go
@@ -21,36 +21,59 @@ func New(repository IProductRepository) *ProductService {
 	return &ProductService{repository: repository}
 }
 
-func (ps ProductService) Create(ctx context.Context, product models.Product) (string, error) {
-	result, err := ps.repository.Create(ctx, product)
+func (ps ProductService) Create(ctx context.Context, productInput ProductServiceInput) (string, error) {
+	newProduct, err := CreateProductDb(productInput)
+	if err != nil {
+		return "", err
+	}
+	result, err := ps.repository.Create(ctx, newProduct)
 	if err != nil {
 		return "", err
 	}
 	return result, nil
 }
 
-func (ps ProductService) Update(ctx context.Context, id string, product models.Product) error {
-	err := ps.repository.Update(ctx, id, product)
+func (ps ProductService) Update(ctx context.Context, id string, productInput ProductServiceInput) error {
+	updateProduct := UpdateProductDb(productInput)
+	err := ps.repository.Update(ctx, id, updateProduct)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (ps ProductService) GetAll(ctx context.Context, filter interface{}) ([]models.Product, error) {
+func (ps ProductService) GetAll(ctx context.Context, filter interface{}) ([]ProductServiceOutput, error) {
 	products, err := ps.repository.GetAll(ctx, filter)
+	var productsOutput []ProductServiceOutput
+	for _, product := range products {
+		productsOutput = append(productsOutput, ProductServiceOutput{
+			Id:          product.Id,
+			Name:        product.Name,
+			Price:       product.Price,
+			Description: product.Description,
+			BuyAt:       product.BuyAt,
+			User:        product.User,
+		})
+	}
 	if err != nil {
 		return nil, err
 	}
-	return products, nil
+	return productsOutput, nil
 }
 
-func (ps ProductService) GetById(ctx context.Context, id string) (models.Product, error) {
+func (ps ProductService) GetById(ctx context.Context, id string) (ProductServiceOutput, error) {
 	product, err := ps.repository.GetById(ctx, id)
 	if err != nil {
-		return models.Product{}, err
+		return ProductServiceOutput{}, err
 	}
-	return product, nil
+	return ProductServiceOutput{
+		Id:          product.Id,
+		Name:        product.Name,
+		Price:       product.Price,
+		Description: product.Description,
+		BuyAt:       product.BuyAt,
+		User:        product.User,
+	}, nil
 }
 func (ps ProductService) Delete(ctx context.Context, id string) error {
 	err := ps.repository.Delete(ctx, id)
