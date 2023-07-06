f6f299ef92033c1d16ce4e028454b31e5f99c6b6
kadr <kadr1987@gmail.com>
2023-07-06 18:44:12 +0400

    теперь коллекция выбирается непосредсвенно для какждого сервиса отдельно

--- cmd/app.go
+++ cmd/app.go
@@ -11,8 +11,10 @@ import (
 	"sync"
 )
 
-const botToken = "5836425300:AAG2azf8sY54f_Y9Mod1PM9vY6IzIWRpnq0"
-const RestApiServerPort = 10000
+const (
+	botToken          = "5836425300:AAG2azf8sY54f_Y9Mod1PM9vY6IzIWRpnq0"
+	RestApiServerPort = 10000
+)
 
 func main() {
 	cfg, err := config.GetConfig()
@@ -20,16 +22,15 @@ func main() {
 		log.Fatal(err)
 	}
 	db, err := repository.NewMongoDB(&repository.Config{
-		Host:       cfg.Db.Mongo.Host,
-		Port:       cfg.Db.Mongo.Port,
-		Database:   cfg.Db.Mongo.Database,
-		Collection: cfg.Db.Mongo.Collection,
+		Host:     cfg.Db.Mongo.Host,
+		Port:     cfg.Db.Mongo.Port,
+		Database: cfg.Db.Mongo.Database,
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	apiHandlers := api.New(db)
+	apiHandlers := api.New(db, cfg)
 	telegramBot, err := telegram.New(botToken, db)
 	if err != nil {
 		log.Fatal(err)
